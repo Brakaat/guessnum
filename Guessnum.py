9b5fc2bf104204e19efa9d98e6c1b48ea9213759
Moszi <niecheeel@gmail.com>
2018-10-04 21:34:57 +0800

    Basic version

--- Guessnum.py
+++ Guessnum.py
@@ -1,14 +1,13 @@
 import random
 random_number = random.randint(1,20)
-user_number = int(input('choose the number:\n'))
+
 while True:
+    user_number = int(input('choose the number:\n'))
     if random_number == user_number:
         print('You are right, the number is : ',random_number)
         break
     elif random_number >= user_number:
         print('You are wrong, the number is bigger, try again')
-        user_number = int(input('choose the number:\n'))
     else:
         print('You are wrong, the number is smaller, try again')
-        user_number = int(input('choose the number:\n'))
 
