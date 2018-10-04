6fa7f68b5b844cf7a9acde87415a60642639e92f
Moszi <niecheeel@gmail.com>
2018-10-04 21:50:41 +0800

    add select MAX & MIN

--- Guessnum.py
+++ Guessnum.py
@@ -1,15 +1,19 @@
 import random
-random_number = random.randint(1,20)
+a = int(input('please select the Minimun'))
+b = int(input('please select the Maximum'))
+print('The random will between ', a,' and', b)
+random_number = random.randint(a,b)
 count = 0
 while True:
     user_number = int(input('choose the number:\n'))
     count += 1
     if random_number == user_number:
         print('You are right, the number is : ',random_number)
-        print('You have tried ',count,' times.')
+        print('You have tried ', count, ' times.')
         break
     elif random_number >= user_number:
         print('You are wrong, the number is bigger, try again')
     else:
         print('You are wrong, the number is smaller, try again')
+    print('This is ', count, 'times.')
 
